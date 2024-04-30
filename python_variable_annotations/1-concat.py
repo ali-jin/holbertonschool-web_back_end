b80d5c61a3437778b9e4b4986f62cf64bf139234
ali-jin <alina.jin.contact@gmail.com>
2024-04-30 12:54:50 +0200

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Basic annotations - concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """A function that concatenate str1 and str2
+
+    Args:
+        str1 (str): The first string
+        str2 (str): The second string
+
+    Returns:
+        str: returns a concatenated string of str1 and str2
+    """
+    return str1 + str2

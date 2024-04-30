70069f3e3e0b90f1813d072900df9253c4d85d85
ali-jin <alina.jin.contact@gmail.com>
2024-04-30 11:46:17 +0200

    add doctring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
+""" Basic annotations - add """
+
 
 def add(a: float, b: float) -> float:
+    """The function return the sum of a and b in float
+
+    Args:
+        a (float): The first float number
+        b (float): The second float number
+
+    Returns:
+        float: the sum of a and b
+    """
 
     return a + b

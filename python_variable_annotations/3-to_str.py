22802e9febdd8cfad983558a217c2408c80e5b35
ali-jin <alina.jin.contact@gmail.com>
2024-05-02 09:47:10 +0200

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string """
+
+
+def to_str(n: float) -> str:
+    """A function that returns the string representation of the float
+
+    Args:
+        n (float): The float number
+    Returns:
+        str: The string representation of the float
+    """
+    return str(n)

7615a7c38f768d7392d195b533ef0c61a9019096
ali-jin <alina.jin.contact@gmail.com>
2024-05-02 10:38:56 +0200

    5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,13 +4,13 @@
 from typing import List
 
 
-def sum_list(input_list) -> List[float]:
-    """_summary_
+def sum_list(input_list: List[float]) -> float:
+    """ Make the sum of the list of floats
 
     Args:
-        input_list (_type_): _description_
+        input_list (List[float]): The list of floats
 
     Returns:
-        list[float]: _description_
+        list[float]: Returns the sum of input_list
     """
     return sum(input_list)

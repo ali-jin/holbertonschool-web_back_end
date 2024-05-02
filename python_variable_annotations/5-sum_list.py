c9966d5b788499defacba140bee77934d9784a2e
ali-jin <alina.jin.contact@gmail.com>
2024-05-02 10:36:32 +0200

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Complex types - list of floats """
+
+from typing import List
+
+
+def sum_list(input_list) -> List[float]:
+    """_summary_
+
+    Args:
+        input_list (_type_): _description_
+
+    Returns:
+        list[float]: _description_
+    """
+    return sum(input_list)

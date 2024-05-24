cab8b6b63b63e42bf80c9f5e8f0175209dc33ef6
ali-jin <alina.jin.contact@gmail.com>
2024-05-24 16:47:15 +0200

    0-basic_cache.py

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Basic dictionary """
+
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """_summary_
+
+    Args:
+        BaseCaching (_type_): _description_
+    """
+    def __init__(self):
+        """ Initialize """
+        self.cache_data = {}
+
+    def put(self, key, item):
+        """ Add item to the cache """
+        if key is None or item is None:
+            return None
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

e8f75bcddd91115a53b59763173f1ad56d471d87
ali-jin <alina.jin.contact@gmail.com>
2024-06-26 09:45:05 +0200

    add log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,41 @@ A module that returns logs
 """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialisation
+
+        Args:
+            fields (List[str]): list of fields to obfuscate
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the logs record
+
+        Args:
+            record (logging.LogRecord): logs record to be formatted
+
+        Returns:
+            str: The formatted message
+        """
+        format_message = super().format(record)
+
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            format_message,
+                            self.SEPARATOR)
 
 
 def filter_datum(
@@ -27,4 +62,4 @@ def filter_datum(
         pattern = f"{field}=[^;]*?{separator}"
         message = re.sub(pattern, f"{field}={redaction}{separator}", message)
 
-    return(message)
+    return message

e8d94066064bb97ef041742092d1276ee5345481
ali-jin <alina.jin.contact@gmail.com>
2024-06-26 09:58:32 +0200

    Connect to secure database

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,16 +2,18 @@
 """
 A module that returns logs
 """
+
 from typing import List
 import re
 import logging
+import os
+import mysql.connector
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+    """Redacting Formatter class"""
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -44,11 +46,8 @@ class RedactingFormatter(logging.Formatter):
 
 
 def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str
-        ) -> str:
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """Replace occurrences of certain field values
 
     Args:
@@ -82,3 +81,20 @@ def get_logger() -> logging.Logger:
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Create and return a MySQL connection.
+
+    Returns:
+        mysql.connector.connection.MySQLConnection: The MySQL connection.
+    """
+    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    return mysql.connector.connect(
+        user=db_username, password=db_password, host=db_host, database=db_name
+    )

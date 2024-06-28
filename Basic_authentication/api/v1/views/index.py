dbe449558f837746ff63edc638bbe333f3b370cc
ali-jin <alina.jin.contact@gmail.com>
2024-06-28 14:05:15 +0200

    Error handler: Forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,13 @@ def unauthorized() -> str:
         abort(401)
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+
+    Return:
+        abort(403)
+    """
+    abort(403)

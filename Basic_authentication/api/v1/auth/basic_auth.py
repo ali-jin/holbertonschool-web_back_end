cd453f7598ddc36080c25848e80a5cc853987e5d
ali-jin <alina.jin.contact@gmail.com>
2024-07-05 13:31:06 +0200

    Basic - Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 BasicAuth class
 """
+import base64
 from .auth import Auth
 
 
@@ -25,3 +26,25 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Decode a base64-encoded authorization header.
+
+        Args:
+            base64_authorization_header (str):The base64-encoded
+                    authorization header.
+
+        Returns:
+            str: The decoded authorization header as a string,
+                    or None if decoding fails.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            message_bytes = base64.b64decode(base64_authorization_header)
+            return message_bytes.decode("utf-8")
+        except Exception:
+            return None

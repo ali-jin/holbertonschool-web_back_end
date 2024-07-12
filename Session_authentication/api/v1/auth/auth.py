9864d19f1dea1b2e1901e8765500d73b51ae0cdd
ali-jin <alina.jin.contact@gmail.com>
2024-07-12 15:11:04 +0200

    update

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth method
+        """
+        if path is None or not excluded_paths:
+            return True
+        path = path + '/' if path[-1] != '/' else path
+        return path not in excluded_paths
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header method
+        """
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user method
+
+        Returns:
+            _type_: _description_
+        """
+        return None

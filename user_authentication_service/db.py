e26e63e80b7a395129d51fd201ea575faec67ef4
ali-jin <alina.jin.contact@gmail.com>
2024-07-26 11:18:53 +0200

    fix pycodestyle in db.py

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,7 +31,15 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add User object
+        """
+        Add a new user to the database.
+
+        Args:
+            email (str): The email address of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The newly created User object.
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)

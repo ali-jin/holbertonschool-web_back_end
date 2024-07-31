e281d3e4a883168e4d77f5145327624e2c0f0570
ali-jin <alina.jin.contact@gmail.com>
2024-07-31 16:23:54 +0200

    add doctring to the _generate_uuid function

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,7 +13,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """ Generate a new uuid """
     return str(uuid.uuid4())
 

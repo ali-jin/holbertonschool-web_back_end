78479b2b0ef7ded29e30c15bbad8886da7eb35a4
ali-jin <alina.jin.contact@gmail.com>
2024-08-01 14:29:37 +0200

    add method to update the user password with the help of the token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -77,3 +77,13 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update the user password """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hashed_password = _hash_password(password)
+        self._db.update_user(
+            user.id, hashed_password=hashed_password, reset_token=None)

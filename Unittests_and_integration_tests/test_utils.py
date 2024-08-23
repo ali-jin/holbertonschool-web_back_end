896560e06077cd1081056d6c501023ac32a49b00
ali-jin <alina.jin.contact@gmail.com>
2024-08-23 16:14:52 +0200

    Unittesting for get_json()

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Unitttests for the utils file"""
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -29,3 +30,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test access_nested_map with exception """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ TestGetJson Class """
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False})
+        ]
+    )
+    @patch('requests.get')
+    def test_get_json(self, url, payload, mock_request):
+        """ Test test_get_json """
+        mock_request.return_value.json.return_value = payload
+        self.assertEqual(get_json(url), payload)
+        mock_request.assert_called_once_with(url)

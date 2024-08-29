44a44ccec2b671d2302a0bc8ddeb5b2eaee590d8
ali-jin <alina.jin.contact@gmail.com>
2024-08-29 15:24:13 +0200

    unittesting for _public_repos_url

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Unitttests for the clients file"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -21,3 +21,12 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(github_org_client.org, {"name": org_name})
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}")
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """ Test _public_repos_url """
+        org_name = "google"
+        repo_url = f"https://api.github.com/orgs/{org_name}"
+        mock_org.return_value = {"repos_url": repo_url}
+        github_org_client = GithubOrgClient(org_name)
+        self.assertEqual(github_org_client._public_repos_url, repo_url)

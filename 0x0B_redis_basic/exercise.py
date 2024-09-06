4a68b11e0c57b452db4e90c44c73f59529faabaa
ali-jin <alina.jin.contact@gmail.com>
2024-09-06 16:24:32 +0200

    add union from typing module and add it to the store method

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,7 @@ Redis exercise
 """
 import redis
 import uuid
+from typing import Union
 
 
 class Cache():
@@ -13,7 +14,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Method that store data in Redis """
         key = str(uuid.uuid4())
         self._redis.set(key, data)

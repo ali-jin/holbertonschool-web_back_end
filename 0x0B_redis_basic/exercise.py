d9c87e2603a23e9882159fdd3d51216dc9baa4ce
ali-jin <alina.jin.contact@gmail.com>
2024-09-06 16:23:09 +0200

    Writing strings to Redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data) -> str:
+    def store(self, data):
         """ Method that store data in Redis """
         key = str(uuid.uuid4())
         self._redis.set(key, data)

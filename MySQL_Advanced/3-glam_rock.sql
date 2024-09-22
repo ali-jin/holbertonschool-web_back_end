ed0ee0c83811bf310051d3553b4cae281a754847
ali-jin <alina.jin.contact@gmail.com>
2024-09-22 19:56:46 +0200

    3-glam_rock.sql

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- Count the number of glam rock bands
-SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
-FROM metal_bands 
-WHERE style LIKE '%Glam rock%';
+SELECT DISTINCT `band_name`,
+                IFNULL(`split`, 2020) - `formed` as `lifespan`
+    FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
+    ORDER BY `lifespan` DESC;

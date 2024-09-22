e8f52daa57c43315cdbe57db7300c57813c71373
ali-jin <alina.jin.contact@gmail.com>
2024-09-22 20:11:40 +0200

    11-need_meeting.sql

---
+++ MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- Creating the view need_meeting
+DROP VIEW IF EXISTS need_meeting;
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < CURDATE() - INTERVAL 1 MONTH);

916494e5c340e64cd9f1dcb4c33a9457b9b398c3
ali-jin <alina.jin.contact@gmail.com>
2024-10-18 10:28:20 +0200

    3-all.js

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import uploadPhoto from "./utils"
+import createUser from "./utils"
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstname} ${value[1].lastname}`);
+    }).catch(() => console.log('Signup system offline'))
+}

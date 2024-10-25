5f997c609a90d9ed94bc91ea80efc73231296f8e
ali-jin <alina.jin.contact@gmail.com>
2024-10-25 11:37:02 +0200

    3-currency.js

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,18 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code =  code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}

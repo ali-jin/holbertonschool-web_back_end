a47798d705efef888951a00f35f2a121db11ef22
ali-jin <alina.jin.contact@gmail.com>
2024-10-25 11:54:30 +0200

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getters and setters
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}

5d752564eeedfcb4d7187da5fbe2d0eb6939e13e
ali-jin <alina.jin.contact@gmail.com>
2024-11-15 15:00:39 +0100

    2-get_students_by_loc.js

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(ListOfStudents, city) {
-  return ListOfStudents.filter(student => student.location === city);
+export default function getStudentsByLocation(arrayStudent, city) {
+  return arrayStudent.filter((student) => student.location === city);
 }

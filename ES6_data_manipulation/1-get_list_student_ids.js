0b3d0fd09d3270a0aaa0cbfae622f1b444fc65f0
ali-jin <alina.jin.contact@gmail.com>
2024-11-15 14:47:36 +0100

    1-get_list_student_ids.js

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(arrayObj) {
   let arrayId = [];
   if (Array.isArray(arrayObj)) {
-    arrayId = [arrayObj[0].id, arrayObj[1].id, arrayObj[2].id]
+    arrayId = arrayObj.map((student) => student.id);
   }
   return arrayId;
 }

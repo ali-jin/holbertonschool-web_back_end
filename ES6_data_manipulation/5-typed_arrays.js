8177985415592f454b53a6279320b89696a54b4f
ali-jin <alina.jin.contact@gmail.com>
2024-11-22 11:02:19 +0100

    5-typed_arrays.js

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,11 @@
-export default function createInt8TypedArray(lenght, position, value) {
-  if (position >= lenght) {
-    throw new Error("Position outside range");
+function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const view = new Int8Array(buffer);
   view[position] = value;
   return new DataView(buffer);
 }
+
+export default createInt8TypedArray;

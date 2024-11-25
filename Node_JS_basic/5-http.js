3ceddd89d64a690a30e2afb015b3876b8c778197
ali-jin <alina.jin.contact@gmail.com>
2024-11-25 15:37:09 +0100

    5-http.js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const DATABASE = args[2];
+const DATABASE = args[0];
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -20,7 +20,7 @@ const app = http.createServer(async (req, res) => {
     res.write('This is the list of our students\n');
     try {
       const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      res.end(`${students}\n`);
     } catch (error) {
       res.end(error.message);
     }
@@ -30,7 +30,7 @@ const app = http.createServer(async (req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  //console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;

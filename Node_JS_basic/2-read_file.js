8ba9e89a9d277c0a89301a560ee41f2f92a2d1c9
ali-jin <alina.jin.contact@gmail.com>
2024-11-25 13:48:41 +0100

    2-read_file.js

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    let students = data.split('\n');
+    students = students.filter((line) => line && !line.startsWith('firstname'));
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = students.reduce((acc, current) => {
+      const [firstname, , , field] = current.split(',');
+      if (field in acc) {
+        acc[field].push(firstname);
+      } else {
+        acc[field] = [firstname];
+      }
+      return acc;
+    }, {});
+
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(
+        `Number of students in ${field}: ${names.length}. List: ${names.join(
+          ', ',
+        )}`,
+      );
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

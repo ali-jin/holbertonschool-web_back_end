0f3e501301291766c49ca39fb3fa8014381f6a7f
ali-jin <alina.jin.contact@gmail.com>
2024-11-27 11:43:54 +0100

    update

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,25 +1,30 @@
 /* eslint-disable */
 const express = require('express');
 const countStudents = require('./3-read_file_async');
+const DebugHolberton = require('./debug');
 
 const app = express();
 const port = 1245;
 
+const d = new DebugHolberton();
+d.fetch();
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
 app.get('/students', async (req, res) => {
   try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`This is the list of our students\n${data}`);
-  } catch (error) {
-    res.send(`This is the list of our students\n${error.message}`);
+    const result = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${result}`);
+    res.status(200);
+  } catch (err) {
+    res.send('This is the list of our students\nCannot load the database');
+    res.status(200);
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
 
 module.exports = app;

7f32e12d21308dd037ce17c454889c34b36105ee
ali-jin <alina.jin.contact@gmail.com>
2024-12-04 15:31:05 +0100

    verify that the function in Utils was used

---
+++ unittests_in_js/3-payment.test.js
@@ -0,0 +1,16 @@
+const sendPaymentRequestToApi = require("./3-payment");
+const Utils = require('./utils');
+const expect = require('chai').expect;
+var sinon = require("sinon");
+
+describe('sendPaymentRequestToApi', function() {
+  it('Validate the usage of the Utils function', () => {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spy.returnValues[0]).to.equal(120);
+
+    expect();
+  })
+})
---
+++ unittests_in_js/utils.js
@@ -0,0 +1,26 @@
+// Function that do arithmetic operations
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof type !== 'string') {
+      throw new Error('Invalid input');
+    }
+  
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
+  
+    if (type === 'SUM') {
+      return roundA + roundB;
+    }
+    else if (type === 'SUBTRACT') {
+      return roundA - roundB;
+    }
+    else if (type === 'DIVIDE') {
+      if (roundB === 0) {
+        return 'Error';
+      }
+      return roundA / roundB;
+    }
+  }
+}
+
+module.exports = Utils;

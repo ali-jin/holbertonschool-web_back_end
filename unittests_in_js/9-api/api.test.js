ccce718187bce57f6fbd2964a14386d60881edf7
ali-jin <alina.jin.contact@gmail.com>
2024-12-06 11:07:03 +0100

    Regex integration testing

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,35 @@
+const chai = require("chai");
+const request = require("request");
+const { response } = require("./api");
+const expect = chai.expect;
+
+describe('Index page', () => {
+  it('should return correct status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return correct result', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('shoud return the correct stautus code when id is a number', (done) => {
+    request('http://localhost:7865/cart/12', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    })
+  })
+
+  it('shoud return the correct stautus code when id is not a number', (done) => {
+    request('http://localhost:7865/cart/hello', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    })
+  })
+});
